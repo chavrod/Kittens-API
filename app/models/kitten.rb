c8c51fd52df8c37714b1a96627f86aedc06f0808
Dima Chavro <dchavro@gmail.com>
2022-08-01 10:30:52 +0100

    Add simple JSON response to index and show actions

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,3 +1,7 @@
 class Kitten < ApplicationRecord
   validates :name, :age, :cuteness, :softness, presence: true
+
+  def as_json(options={})
+    super(:only => [:id, :name, :age, :cuteness, :softness])
+  end
 end
